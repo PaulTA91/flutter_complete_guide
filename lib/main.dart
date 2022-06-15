05b38a8136bd270ba0a3d474ef62fec8b9e436e3
PaulTA91 <parmst202@caledonian.ac.uk>
2022-06-15 12:34:47 +0100

    Widget tree and layout basics

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,35 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Text('Hello World!'));
+    var questions = [
+      "What's your favourite colour?",
+      "What's your facourite animal?"
+    ];
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App'),
+        ),
+        body: Column(
+          children: [
+            Text("The question!"),
+            RaisedButton(
+              child: Text("Answer 1"),
+              onPressed: null,
+            ),
+            RaisedButton(
+              child: Text("Answer 2"),
+              onPressed: null,
+            ),
+            RaisedButton(
+              child: Text("Answer 3"),
+              onPressed: null,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
