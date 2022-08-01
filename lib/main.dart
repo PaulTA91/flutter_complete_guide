566f72fb2da4dd46d636a774222e4229b7ff811f
PaulTA91 <parmst202@caledonian.ac.uk>
2022-08-01 17:52:31 +0100

    escaping nullSafety

--- lib/main.dart
+++ lib/main.dart
@@ -14,30 +14,33 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   var _questionIndex = 0;
+  static const questions = [
+    {
+      'questionText': "What's your favourite colour?",
+      'answers': ['Red', 'Blue', 'Purple', 'Green']
+    },
+    {
+      'questionText': "What's your favourite animal?",
+      'answers': ['Dog', 'Cat', 'Giraffe', 'Shark']
+    },
+    {
+      'questionText': "What's your favourite food?",
+      'answers': ['Borgor', 'Pasta', 'Ice Cream', 'Pizza']
+    },
+  ];
 
   void _answerQuestion() {
     setState(() {
       _questionIndex++;
       print(_questionIndex);
+      if (_questionIndex < questions.length) {
+        print('We have more questions');
+      }
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    var questions = [
-      {
-        'questionText': "What's your favourite colour?",
-        'answers': ['Red', 'Blue', 'Purple', 'Green']
-      },
-      {
-        'questionText': "What's your favourite animal?",
-        'answers': ['Dog', 'Cat', 'Giraffe', 'Shark']
-      },
-      {
-        'questionText': "What's your favourite food?",
-        'answers': ['Borgor', 'Pasta', 'Ice Cream', 'Pizza']
-      },
-    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
@@ -45,7 +48,7 @@ class _MyAppState extends State<MyApp> {
         ),
         body: Column(
           children: [
-            Question(questions[_questionIndex]['questionText']),
+            Question(questions[_questionIndex]['questionText'] as String),
             ...(questions[_questionIndex]['answers'] as List<String>)
                 .map((answer) {
               return Answer(_answerQuestion, answer);
